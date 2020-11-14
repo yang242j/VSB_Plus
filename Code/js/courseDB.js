d9fa44cba4faa7853494a933e480f4edead0df47
Nick-liu666 <55857483+Nick-liu666@users.noreply.github.com>
2020-11-14 00:27:12 -0600

    update courseDB

---
+++ Code/js/courseDB.js
@@ -0,0 +1,9 @@
+function setFilter(){
+    var program = document.getElementById("program").value;
+    var faculty = document.getElementById("faculty").value;
+    document.getElementById("filter_cond").innerHTML = "Program: " + program + "<br> Faculty: " + faculty;
+}
+
+function overCourse(){
+    
+}
