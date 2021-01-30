209487afda07b94e923c48545d9087b10106ffa8
mingchipc <44107118+mingchipc@users.noreply.github.com>
2021-01-30 00:34:29 -0600

    Create academicBuilder.js
    function for the four hide buttons

---
+++ Code/js/academicBuilder.js
@@ -0,0 +1,44 @@
+function hideAndShow1() {
+  var x = document.getElementById("Term");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+ 
+}
+
+function hideAndShow2() {
+  var x = document.getElementById("term");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+ 
+}
+
+function hideAndShow3() {
+  var x = document.getElementById("complete");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+ 
+}
+
+function hideAndShow4() {
+  var x = document.getElementById("not_complete");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+ 
+}
+
