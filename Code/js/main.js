812664c563b3d99859c4dfa5e872224530f3289f
yang242j <yang242j@uregina.ca>
2021-01-29 22:16:50 -0600

    user info display

--- Code/js/main.js
+++ Code/js/main.js
@@ -1,17 +1,15 @@
 /* When the user clicks on the nav-right buttons, 
-toggle between hiding and showing the dropdown content */
-function dropdownFunc() {
-    document.getElementById("dropdown-menu").style.display = "block";
+toggle between hiding and showing the addOn content */
+function addonFunc() {
+    document.getElementById("addon-menu").style.display = "block";
 }
 
-// Close the dropdown if the user clicks outside of it
-var dropbtn = document.getElementById("dropbtn");
-var usrimg = document.getElementById("usrimg");
+// Close the addon if the user clicks outside of it
 var usertext = document.getElementById("usertext");
 window.onclick = function(event) {
-    if (event.target != dropbtn && event.target != usrimg && event.target != usertext) { 
-        document.getElementById("dropdown-menu").style.display = "none";
+    if (event.target != usertext) { 
+        document.getElementById("addon-menu").style.display = "none";
         //console.log(dropbtn);
     }
-    //console.log(document.getElementById("dropdown-menu").style.display);
+    //console.log(document.getElementById("addon-menu").style.display);
 }
