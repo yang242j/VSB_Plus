cb2ad7b4aa3d9a34a76eb3658f1d8fdab4fc47d8
yang242j <yang242j@uregina.ca>
2021-02-02 18:48:24 -0600

    Update login.php

--- Code/login.php
+++ Code/login.php
@@ -56,6 +56,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 if (mysqli_stmt_num_rows($stmt) == 1) {
                     // Bind result variables
                     mysqli_stmt_bind_result($stmt, $studentid, $name, $hashed_password);
+                    echo $stmt;
+                    echo $studentid;
+                    echo $name;
+                    echo $hashed_password;
                     if (mysqli_stmt_fetch($stmt)) {
                         if (password_verify($password, $hashed_password)) {
                             // Password is correct, so start a new session
