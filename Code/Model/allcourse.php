cafd2f617c8fa8b9a0f31f811dfe9fb084acd4a4
Nick-liu666 <55857483+Nick-liu666@users.noreply.github.com>
2021-02-04 09:00:23 -0600

    Update allcourse.php

--- Code/Model/allcourse.php
+++ Code/Model/allcourse.php
@@ -1,6 +1,6 @@
 <?php
-    // $host = "localhost";
-    $host = "15.223.123.122";
+    $host = "localhost";
+    // $host = "15.223.123.122";
     $username = 'root';
     $pass = "vsbp";
     $database = "course";
