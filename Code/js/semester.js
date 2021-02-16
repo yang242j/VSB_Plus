89f6c80a65f508e72e45d65d6e11fd9b29e6e2f2
yang242j <yang242j@uregina.ca>
2021-02-15 20:43:26 -0600

    Update semester.js

--- Code/js/semester.js
+++ Code/js/semester.js
@@ -1,5 +1,5 @@
 const colors = ["lightblue", "lightseagreen", "pink", "yellow", "Azure", "Bisque", "Coral", "Cyan", "Cornsilk", "Lavender"];
-var pre_colorID = "", examDateList = ["Apr 20 2021"];
+var pre_colorID = "", examDateList = [new Date("Apr 20 2021")];
 
 //Calendar init
 var calendarEl = document.getElementById('calendar');
