dc48f1e09ec29e4c12a3d3ade943e8c76fd49061
yang242j <yang242j@uregina.ca>
2021-02-15 19:23:56 -0600

    Update semester.js

--- Code/js/semester.js
+++ Code/js/semester.js
@@ -196,12 +196,9 @@ function appendCalendar(section_num, short_name, time, days, date_range, BGC) {
     var event_title = event_id.concat(" [", section_num, "]");
     var start_date = new Date(date_range.slice(0, 12)).toISOString().substring(0, 10);
     var end_date = new Date(date_range.slice(15)).toISOString().substring(0, 10);
-    var start_time = time.slice(0, 7);
-    var end_time = time.slice(10);
+    var start_time = get24HrsFrm12Hrs(time.slice(0, 7));
+    var end_time = get24HrsFrm12Hrs(time.slice(10));
     var daysOfWeek = [];
-
-    console.log(start_time);
-    console.log(get24HrsFrm12Hrs(start_time));
 
     // Convert the daysOfWeek
     for (var i = 0; i < days.length; i++) {
@@ -236,8 +233,8 @@ function appendCalendar(section_num, short_name, time, days, date_range, BGC) {
             title: event_title,
             startRecur: start_date,
             endRecur: end_date,
-            startTime: "8:00pm",
-            endTime: "9:00pm",
+            startTime: start_time,
+            endTime: end_time,
             daysOfWeek: daysOfWeek,
             textColor: "black",
             color: BGC,
