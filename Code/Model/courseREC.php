5994292bf45635d4034d89c333ac69b29d9dc6a4
yang242j <yang242j@uregina.ca>
2021-02-16 19:58:10 -0600

    Update courseREC.php

--- Code/Model/courseREC.php
+++ Code/Model/courseREC.php
@@ -23,6 +23,8 @@ if (isset($_REQUEST["term"]) and $_REQUEST["term"] != '') {
 }
 
 if ($doneList !== "" && $major !== "" && $term !== "") {
+
+    $toTakeList = array();
     // 1. Based on the major, open and read the json file of required courses.
     $fileName = $major . '_req.json';
     $json_String = file_get_contents("../JSON/$fileName"); // Get the contents of the JSON file 
@@ -32,11 +34,17 @@ if ($doneList !== "" && $major !== "" && $term !== "") {
 
     // 2. Compare the doneList and the reqList_json, store the first 10 courses into an array.
     foreach ($json_array as $reqTerm => $reqCourseArray) {
-        echo $reqTerm;
+        echo "$reqTerm: <br>";
         foreach ($reqCourseArray as $reqCourse) {
-            echo $reqCourse;
+            //echo $reqCourse;
+            if (in_array($reqCourse, $doneList)) {
+                echo "$reqCourse done <br>";
+            } else {
+                array_push($reqCourse, $toTakeList);
+            }
         }
     }
+    echo $toTakeList;
 
 } else {
     echo "One of three inputs is invalid";
