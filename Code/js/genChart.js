cf5bca690a2216b77a8f3ffa71b4ecc922485f6e
Nick-liu666 <55857483+Nick-liu666@users.noreply.github.com>
2021-02-18 22:10:58 -0600

    update

--- Code/js/genChart.js
+++ Code/js/genChart.js
@@ -132,7 +132,7 @@ function genChart1(data, divId){
   data = {
     datasets: [{
         data: [goodGrade, generalGrade, poorGrade, notPass, left_class],
-        backgroundColor: ['rgba(255, 99, 132, 0.2)',
+        backgroundColor: ['#FF6666',
         'rgba(54, 162, 235, 0.2)',
         'rgba(255, 206, 86, 0.2)',
         'rgba(75, 192, 192, 0.2)',
