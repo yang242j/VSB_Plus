8683b189000535b554eae77dab08be013231c7a5
yang242j <yang242j@uregina.ca>
2021-02-20 15:07:00 -0600

    Update semester.js

--- Code/js/semester.js
+++ Code/js/semester.js
@@ -78,7 +78,7 @@ function dropL(ev, term) {
                 var lec_exam_id='0', lab_id='0'; // Init common section variables
 
                 //3.1.Fetch Lecture Section JSON data
-                let lec_json_obj = $.post('Model/section.php', { short_name: short_name, schedule_type: "Lecture", term: term }, function (result2) {
+                $.post('Model/section.php', { short_name: short_name, schedule_type: "Lecture", term: term }, function (result2) {
                     test_obj = JSON.parse(result2);
                     console.log("Test: " + test_obj);
                     //alert("Lecture: " + Object.keys(test_obj).length);
