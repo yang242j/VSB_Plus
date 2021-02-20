c27662da84bca20bb00625dbd8b9f8994a20a095
yang242j <yang242j@uregina.ca>
2021-02-20 15:48:00 -0600

    Update semester.js

--- Code/js/semester.js
+++ Code/js/semester.js
@@ -79,10 +79,12 @@ function dropL(ev, term) {
 
                 //3.1.Fetch Lecture Section JSON data
                 let lec_json_obj = $.post('Model/section.php', { short_name: short_name, schedule_type: "Lecture", term: term }, function (result2) {
-                    //var obj = JSON.parse(result2);
-                    //console.log("Test: " + obj);
+                    var obj = JSON.parse(result2);
+                    console.log("Test: " + obj);
                     //alert("Lecture: " + Object.keys(obj).length);
                 });
+                console.log(lec_json_obj);
+                console.log(JSON.parse(lec_json_obj));
 
                 //3.2. Fetch Lab Section JSON data
                 let lab_json_obj = $.post('Model/section.php', { short_name: short_name, schedule_type: "Lab", term: term }, function(result3) {
@@ -103,7 +105,7 @@ function dropL(ev, term) {
                 //4.Append cards, calendars, exams
                 appendCourseCard(course_json, BGC); //4.1.Append courseCard-list
 
-                console.log(JSON.parse(lec_json_obj));
+                //console.log(lec_json_obj);
 
                 if (lec_json_obj[lec_exam_id]) {
                     appendCalendar(lec_json_obj[lec_exam_id], BGC); //4.2.1.Append lecture calendar event
