958d7a35a576919d8bc53befef943626ca3a3918
yang242j <yang242j@uregina.ca>
2021-02-24 20:29:16 -0600

    Update semester.js

--- Code/js/semester.js
+++ Code/js/semester.js
@@ -26,7 +26,7 @@ var calendar = new FullCalendar.Calendar(calendarEl, {
     events: [],
 });
 calendar.render();
-calendar.setOption('aspectRatio', 1.0);
+//calendar.setOption('aspectRatio', 0.9);
 
 document.body.onkeydown = function (ev) {
     let shadowIsOn = document.getElementById("shadowLayer").style.display == "block";
