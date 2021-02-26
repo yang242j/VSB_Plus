63b7138f857f43abd2d9af19a077b8773a215137
xia <huaxiairving@Gmail.com>
2021-02-26 01:16:35 -0600

    test

--- Code/js/academicCustomizeBuilder.js
+++ Code/js/academicCustomizeBuilder.js
@@ -1,7 +1,7 @@
 
 function fetchCourseJSON() {
     alert("asdasdsa");
-    /*return $.post('Model/takenClass.php', { sid: 200362586 , password: 200362586}, function (data) {});*/
+    return $.post('Model/takenClass.php', { sid: 200362586 , password: 200362586}, function (data) {});
 }
 
 fetchCourseJSON();
