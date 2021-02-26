57efba4830a2ba57217215f7bc92f909f6f17eb4
xia <huaxiairving@Gmail.com>
2021-02-26 03:35:03 -0600

    test

--- Code/academicBuilder_Builder.php
+++ Code/academicBuilder_Builder.php
@@ -38,7 +38,8 @@ session_start();
 <body>
 <script src ="js/academicCustomizeBuilder.js" ></script>
    <a href='Model/takenClass.php' method = "post">takenss</a> 
-   <input type = "hidden" id = "studentId" value = "<?= htmlspecialchars($sid)?>">
+   <input type = "hidden" id = "studentId" value = "<?php echo htmlspecialchars($_SESSION["name"]); ?>">
+   
 <?php
     // Check if the user is logged in, if not then hide nav-right div
     if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
