105a283bcd28fa072ea6fffb9c708f9d251ae277
yang242j <yang242j@uregina.ca>
2021-02-27 17:55:04 -0600

    Update courseREC.php

--- Code/Model/courseREC.php
+++ Code/Model/courseREC.php
@@ -90,4 +90,21 @@ function isSectionEmpty($path) {
     //echo json_encode($parsed_json, JSON_PRETTY_PRINT);
 }
 
+function getCourseJSON($short_name) {
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL,"course.php");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "short_name=".$short_name);
+
+    // Receive server response ...
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $server_output = curl_exec($ch);
+
+    echo $server_output;
+
+    curl_close ($ch);
+}
+
 ?>
