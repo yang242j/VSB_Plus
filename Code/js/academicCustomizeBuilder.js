0cb9b363af3d17cef7e86be9c2c2ab3b83acef93
xia <huaxiairving@Gmail.com>
2021-02-28 15:55:37 -0600

    test

--- Code/js/academicCustomizeBuilder.js
+++ Code/js/academicCustomizeBuilder.js
@@ -1,32 +1,35 @@
 var studentData;
 function fetchCourseJSON(sid) {
     // alert(sid);
-   $.post('Model/takenClass.php', { sid: sid, password: sid }, function (data) {
-       test(data);
-           
+    $.post('Model/takenClass.php', { sid: sid, password: sid }, function (data) {
+        test(data);
+        showCourses(data);
     });
 }
 
-window.onload = function init() { 
+window.onload = function init() {
     fetchCourseJSON(getSid());
 }
 function getSid() {
     var sid = document.getElementById("userId").innerHTML;
     return sid;
 }
-function test(data){
+function test(data) {
     var btn = document.getElementById("p1");
     var dataJSON = JSON.parse(data);
     console.log(dataJSON);
-    btn.onclick = function (){
+    btn.onclick = function () {
 
         console.log(dataJSON[0].course_ID);
     }
 }
-function showCourses(data){
+function showCourses(data) {
     var dataJSON = JSON.parse(data);
-    for (i =0; i < 12; i++){
-document.getElementById("tc"+i).innerHTMl = dataJSON[i].course_ID;
+    for (i = 0; i < 12; i++) {
+        document.getElementById("tc" + i).innerHTMl = " ";
+        if (i > dataJSON.length) {
+            document.getElementById("tc" + i).innerHTMl = dataJSON[i].course_ID;
+        }
     }
 
 }
