73fbe5960d2e96ad57f542d62a7903801a48b03e
xia <huaxiairving@Gmail.com>
2021-02-28 16:44:28 -0600

    test

--- Code/js/academicCustomizeBuilder.js
+++ Code/js/academicCustomizeBuilder.js
@@ -1,4 +1,5 @@
 var studentData;
+var courseReqData;
 function fetchCourseJSON(sid) {
     // alert(sid);
     $.post('Model/takenClass.php', { sid: sid, password: sid }, function (data) {
@@ -6,7 +7,7 @@ function fetchCourseJSON(sid) {
         showCourses(data);
     });
 }
-
+getTermData();
 window.onload = function init() {
     fetchCourseJSON(getSid());
 }
@@ -14,10 +15,23 @@ function getSid() {
     var sid = document.getElementById("userId").innerHTML;
     return sid;
 }
+function getTermData() {
+    var myRequest = new XMLHttpRequest;
+    var faculty = "ESE";
+    var url = "JSON/reqCourse/" + faculty + "_req.json";
+    myRequest.open("GET", url, false);
+    myRequest.onload = function () {
+        var data = JSON.parse(myRequest.responseText);
+        courseReqData = data;
+        console.log(termData);
+    }
+    myRequest.send();
+}
+
 function test(data) {
     var btn = document.getElementById("p1");
     var dataJSON = JSON.parse(data);
-    console.log(dataJSON);
+    //console.log(dataJSON);
     btn.onclick = function () {
        console.log(dataJSON[0]);
     }
