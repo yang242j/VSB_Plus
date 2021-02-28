84d236aa7dafef9a699a9a2137d9ea0cc25e7174
yang242j <yang242j@uregina.ca>
2021-02-27 18:14:34 -0600

    Update test.php

--- Code/Model/test.php
+++ Code/Model/test.php
@@ -1,11 +1,14 @@
 <?php
 
-$response = get_web_page("http://15.223.123.122/vsbp/Code/Model/course.php?short_name=ENGG%20401");
+$response = get_web_page("ENGG 401");
 $resArr = array();
 $resArr = json_decode($response);
 echo "<pre>"; print_r($resArr); echo "</pre>";
 
-function get_web_page($url) {
+function get_course_json($short_name) {
+    $url = "http://15.223.123.122/vsbp/Code/Model/course.php";
+    $postField = "short_name=$short_name";
+
     $options = array(
         CURLOPT_RETURNTRANSFER => true,   // return web page
         CURLOPT_HEADER         => false,  // don't return headers
@@ -16,6 +19,7 @@ function get_web_page($url) {
         CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
         CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
         CURLOPT_TIMEOUT        => 120,    // time-out on response
+        CURLOPT_POSTFIELDS     => $postField, // set up post fields
     ); 
 
     $ch = curl_init($url);
