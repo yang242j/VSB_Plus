155ce03371b04d4253fc28ae6fc0b1692573f802
xia <huaxiairving@Gmail.com>
2021-02-28 17:12:27 -0600

    test

--- Code/js/academicCustomizeBuilder.js
+++ Code/js/academicCustomizeBuilder.js
@@ -31,41 +31,40 @@ function test(data) {
     var dataJSON = JSON.parse(data);
     //console.log(dataJSON);
     btn.onclick = function () {
-       console.log(dataJSON[0]);
+        console.log(dataJSON[0]);
     }
 }
-function findCourseToTake(data){
-      /*console.log(data);
-      console.log(courseReqData);*/
-      var courseCompleted = [];
-      var courseNotCompleted = [];
-      for (i = 0; i<data.length;i++){
-          courseCompleted[i] = data[i].course_ID;
-      }
-      console.log(courseCompleted);
-      for (term in courseReqData) {
+function findCourseToTake(data) {
+    /*console.log(data);
+    console.log(courseReqData);*/
+    var courseCompleted = [];
+    var courseNotCompleted = [];
+    for (i = 0; i < data.length; i++) {
+        courseCompleted[i] = data[i].course_ID;
+    }
+    console.log(courseCompleted);
+    for (term in courseReqData) {
         if (courseReqData[term][i] != "Approved") {
-            for (i=0;i<courseReqData[term].length;i++)
-            courseNotCompleted[i] = courseReqData[term][i];
-            }
+            for (i = 0; i < courseReqData[term].length; i++)
+                courseNotCompleted[i] = courseReqData[term][i];
         }
-      console.log(courseNotCompleted);
     }
-
+    console.log(courseNotCompleted);
 }
+
 
 function showCourses(data) {
     var dataJSON = JSON.parse(data);
     findCourseToTake(dataJSON);
     for (i = 0; i < 12; i++) {
         if (i < dataJSON.length) {
-        document.getElementById("ct" + i).innerHTML = dataJSON[i].course_ID;
+            document.getElementById("ct" + i).innerHTML = dataJSON[i].course_ID;
         }
-        else{
-        document.getElementById("ct" + i).innerHTML = " ";
+        else {
+            document.getElementById("ct" + i).innerHTML = " ";
         }
 
-        
+
     }
 
 }
