63ed9f9a17508a1c6e6d7c0713c2801fae027ac4
xia <huaxiairving@Gmail.com>
2021-02-28 19:17:21 -0600

    test

--- Code/js/academicCustomizeBuilder.js
+++ Code/js/academicCustomizeBuilder.js
@@ -65,8 +65,8 @@ function showCourses(data) {
     /*console.log(notCompletedData);*/
     for (i = 0; i < 12; i++) {
         if (i < dataJSON.length) {
-            if (completedData[i + 12 * counterForCompleted].final_grade != "NP"
-                && completedData[i + 12 * counterForCompleted].final_grade != "W"){
+            if (cdataJSON[i].final_grade != "NP"
+                && dataJSON[i].final_grade != "W"){
 
                 document.getElementById("ct" + i).innerHTML = dataJSON[i].course_ID;
                 }
