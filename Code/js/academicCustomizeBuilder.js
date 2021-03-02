567cada0a3ce9e4d638e100d6774817ae536b7a3
xia <huaxiairving@Gmail.com>
2021-03-02 01:39:38 -0600

    asds

--- Code/js/academicCustomizeBuilder.js
+++ Code/js/academicCustomizeBuilder.js
@@ -7,7 +7,7 @@ function fetchCourseJSON(sid,password) {
     $.post('Model/takenClass.php', { sid: sid, password: password }, function (data) {
         btnForCourse(data);
         showCourses(data);
-        console.log(data);
+        //console.log(data);
     });
 }
 getTermData("ESE");
@@ -125,7 +125,8 @@ function showCourses(data) {
     //console.log(dataJSON);
     for (i = 0; i < 12; i++) {
         if (i < dataJSON.length) {
-            document.getElementById("ct" + i).innerHTML = dataJSON[i].course_ID + " <br/> " +dataJSON[i].term;
+            //<br/>
+            document.getElementById("ct" + i).innerHTML = dataJSON[i].course_ID + " " +dataJSON[i].term;
             document.getElementById("ct" + i).style.color = getColor(i,dataJSON);
 
         }
@@ -179,7 +180,7 @@ function btnForCourse(data) {
                     return;
                 }
                 else {
-                    document.getElementById("ct" + i).innerHTML = completedData[i + 12 * counterForCompleted].course_ID + "<br/> "+ completedData[i + 12 * counterForCompleted].term;
+                    document.getElementById("ct" + i).innerHTML = completedData[i + 12 * counterForCompleted].course_ID + " "+ completedData[i + 12 * counterForCompleted].term;
                     document.getElementById("ct" + i).style.color = getColor(i,dataJSON);
                 }
             }
@@ -204,7 +205,7 @@ function btnForCourse(data) {
                     return;
                 }
                 else {
-                    document.getElementById("ct" + i).innerHTML = completedData[i + 12 * counterForCompleted].course_ID + "<br/>"+ completedData[i + 12 * counterForCompleted].term;
+                    document.getElementById("ct" + i).innerHTML = completedData[i + 12 * counterForCompleted].course_ID + ""+ completedData[i + 12 * counterForCompleted].term;
                     document.getElementById("ct" + i).style.color = getColor(i,dataJSON);
                 }
             }
