e3aab59d8e958e2f768e69ddf3872ed3cac6f33a
xia <huaxiairving@Gmail.com>
2021-03-01 23:29:54 -0600

    asdas

--- Code/js/academicCustomizeBuilder.js
+++ Code/js/academicCustomizeBuilder.js
@@ -7,9 +7,7 @@ function fetchCourseJSON(sid) {
     $.post('Model/takenClass.php', { sid: sid, password: sid }, function (data) {
         btnForCourse(data);
         showCourses(data);
-
         console.log(data);
-        showTerm(data);
     });
 }
 getTermData("ESE");
