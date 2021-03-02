001cfda3fb8cc05c3a0b924039188d9eee042a3e
xia <huaxiairving@Gmail.com>
2021-03-01 22:13:18 -0600

    asdas

--- Code/js/academicCustomizeBuilder.js
+++ Code/js/academicCustomizeBuilder.js
@@ -7,7 +7,7 @@ function fetchCourseJSON(sid) {
     $.post('Model/takenClass.php', { sid: sid, password: sid }, function (data) {
         btnForCourse(data);
         showCourses(data);
-    
+
         console.log(data);
         showTerm(data);
     });
@@ -228,12 +228,24 @@ function btnForCourse(data) {
 //show terms 
 function showTerm(data) {
     var dataJSON = JSON.parse(data);
-    var term = document.getElementById("term1");
-    color = "red";
-    term.innerHTML = "<div class = 'tittle'>" + "<h2>"+ "Term3:" + "</h2></div>" +
-    "<div class = 'course_cards' id = 'course_cards_builder' style = 'border-color:" + color + "'>" + "<h3>" + "ENSE 477"+ "</h3>" +
-    "<p>"+ "Mechanics for EngineersDynamics asdasXSaas"+"</p>" +
-    "</div>";
+    var term1 = document.getElementById("term1");
+
+    for (i = 0; i < dataJSON.length; i++) {
+        if (dataJSON[i].final_grade == "NP" || dataJSON[i].final_grade == "W") {
+            delete dataJSON[i];
+        }
+    }
+    dataJSON.sort();
+    var courseFullName = "Mechanics for EngineersDynamics asdasXSaas";
+    var color = "red";
+    if (dataJSON[0].final_grade == "NP")
+    {
+        color = "blue";
+    }
+    term1.innerHTML = "<div class = 'tittle'>" + "<h2>" + dataJSON[0].term + "</h2></div>" +
+        "<div class = 'course_cards' id = 'course_cards_builder' style = 'border-color:" + color + "'>" + "<h3>" + "ENSE 477" + "</h3>" +
+        "<p>" + courseFullName + "</p>" +
+        "</div>";
 
 }
 
