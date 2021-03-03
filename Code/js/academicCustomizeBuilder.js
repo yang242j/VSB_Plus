88262cc5820fc604c9c0329775855cacf576be2a
xia <huaxiairving@Gmail.com>
2021-03-03 11:21:00 -0600

    s

--- Code/js/academicCustomizeBuilder.js
+++ Code/js/academicCustomizeBuilder.js
@@ -140,14 +140,13 @@ function showCourses(data) {
             "<div class = 'courseTags' >" +
             "<div title= 'Hooray!' draggable = 'true' id ='nct" + i + "'" + ">" +
             notCompletedData[i] +
-            "<p id ='nnct" + i + "'" + ">" + getTermInfo(notCompletedData[i]) +"</p>" +
-            "<p 'hidden' id ='nnnct" + i + "'" + ">" + getPrerequisite(notCompletedData[i]) +"</p>" +
+            "<p id ='nnct" + i + "'" + ">" + getPrerequisite(notCompletedData[i]) +"</p>" +
+    
             "</div> </div>";
         document.getElementById("nnct"+i).style.visibility = "hidden";
-        document.getElementById("nnct"+i).style.fontSize = "8px";
+        document.getElementById("nnct"+i).style.fontSize = "4px";
         document.getElementById("nct"+i).style.color="black";
 
-        document.getElementById("nnnct"+i).style.fontSize = "1px";
         // reset the color after a short delay
         //$("#nct"+ i).tooltip();  
     }
