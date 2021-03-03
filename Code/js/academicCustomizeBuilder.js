6a720dde49bc5543390aa4995c5c613d1fa00194
xia <huaxiairving@Gmail.com>
2021-03-03 06:07:22 -0600

    s

--- Code/js/academicCustomizeBuilder.js
+++ Code/js/academicCustomizeBuilder.js
@@ -143,7 +143,7 @@ function showCourses(data) {
             "<p id ='nnct" + i + "'" + ">" + getTermInfo(notCompletedData[i]) + "</p>" +
             "</div> </div>";
         document.getElementById("nnct"+i).style.visibility = "hidden";
-        document.getElementById("nct"+i).style.borderColor="#D4F19F";
+        document.getElementById("nct"+i).style.backgroundColor="#D4F19F";
 
     }
     for (i = 0; i < notCompletedData.length; i++) {
