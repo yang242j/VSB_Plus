f1d7bfa927636d3457d31be7e9f0b8ef3bbfdb34
xia <huaxiairving@Gmail.com>
2021-03-03 11:49:39 -0600

    s

--- Code/js/academicCustomizeBuilder.js
+++ Code/js/academicCustomizeBuilder.js
@@ -118,11 +118,10 @@ function showCourses(data) {
     /*console.log(dataJSON);
     console.log(courseReqData);
     console.log(notCompletedData);*/
-    console.log(getPrerequisite(notCompletedData[0]));
-    console.log(getPrerequisite(notCompletedData[1]));
-    console.log(getPrerequisite(notCompletedData[2]));
-    console.log(getPrerequisite(notCompletedData[3]));
-    console.log(getPrerequisite(notCompletedData[4]));
+    for(i=0;i<10;i++){
+    console.log(getPrerequisite(notCompletedData[i]));
+    }
+   
 
     /*for (i = 0; i < 12; i++) {
         document.getElementById("ct" + i).innerHTML = " ";
