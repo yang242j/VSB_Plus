87dfb6638ba457e856597b181d373157f4b6f7e9
xia <huaxiairving@Gmail.com>
2021-03-03 04:24:57 -0600

    s

--- Code/js/academicCustomizeBuilder.js
+++ Code/js/academicCustomizeBuilder.js
@@ -140,7 +140,7 @@ function showCourses(data) {
             "<div class = 'courseTags' >" +
             "<div draggable = 'true' id ='nct" + i + "'" + ">" +
             notCompletedData[i] +
-            "<p hidden id ='nnct" + i + "'" + ">" + getTermInfo(notCompletedData[i]) + "</p>" +
+            "<p hidden id ='nnct" + i + "'" + ">" + notCompletedData[i] + "</p>" +
             "</div> </div>";
         document.getElementById("courseTagArea");
     }
