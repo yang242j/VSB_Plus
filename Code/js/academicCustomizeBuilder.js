abb56ca3a75432af7f98a23c2657685fd11770fc
xia <huaxiairving@Gmail.com>
2021-03-03 12:40:51 -0600

    s

--- Code/js/academicCustomizeBuilder.js
+++ Code/js/academicCustomizeBuilder.js
@@ -355,7 +355,7 @@ function dragTest(elementId) {
             //var content = document.getElementById(newForAlern).innerHTML;
 
             document.getElementById(newForAlern).style.visibility = "visible";
-            console.log(newForAlern2.innerHTML);
+            console.log(newForAlern2.droppedElementId);
 
 
             dropZone.appendChild(droppedElement);
