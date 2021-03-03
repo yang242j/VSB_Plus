45502ff422b7a57ee47c04e239cd2c2a140de024
xia <huaxiairving@Gmail.com>
2021-03-03 05:34:12 -0600

    s

--- Code/academicBuilder_Builder.php
+++ Code/academicBuilder_Builder.php
@@ -53,7 +53,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/academicBuilder_Default.css">
+    <link rel="stylesheet" href="css/academicBuilder_Customize.css">
 
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
