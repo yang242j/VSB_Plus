c8a65288ffb0eeecbf9097c4b53a1ab3d398ef5d
yang242j <yang242j@uregina.ca>
2021-03-06 14:10:20 -0600

    Update courseRegStatus.php

--- Code/Model/courseRegStatus.php
+++ Code/Model/courseRegStatus.php
@@ -39,8 +39,9 @@ if (preg_match_all("/([a-z]+\s[0-9]+)/i", $courseid) == 1){
         "Completion" => false,  // {bool} whether this course has already completed.
         "Availability" => false, // {bool} whether this course is available at the given term.
         "Prerequisites" => false, // {bool} whether this course has matched all prerequisites.
-        "Notes" => "<b>$courseid: </b>\n" // {string} Additional notes.
+        "Notes" => "" // {string} Additional notes.
     );
+    $status["Notes"] += "<b>$courseid: </b>\n";
     // 3.1 Check if the course is in doneList
     if ( in_array($courseid, $doneList) ) {
         $status["Completion"] = true;
