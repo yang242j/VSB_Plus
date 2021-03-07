4124280dcb3b0d1b23fa5e366a8b5bb96eebf1c2
mingchipc <44107118+mingchipc@users.noreply.github.com>
2021-03-07 17:36:57 -0600

    Update academicBuilder_Default.php

--- Code/academicBuilder_Default.php
+++ Code/academicBuilder_Default.php
@@ -187,6 +187,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
         <div class = "course_cards"></div>
         <div class = "course_cards"></div>
         <div class = "course_cards"></div>
+        <div class = "course_cards"></div>
         </div>
 
     <div class = "term1" id ="term9">
