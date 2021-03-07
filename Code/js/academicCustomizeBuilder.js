433b9440ae08cb1d810b877b73fc6e0973fce344
xia <huaxiairving@Gmail.com>
2021-03-07 00:27:16 -0600

    tes t

--- Code/js/academicCustomizeBuilder.js
+++ Code/js/academicCustomizeBuilder.js
@@ -282,9 +282,9 @@ function getTermInfo(courseName) {
     url1 = "JSON/202110/" + courseName + ".json";
 
     myRequest3.open("GET", url1, false);
+    console.log(myRequest3.response);
     myRequest3.onload = function () {
         if (myRequest3.status==200 && myRequest3.responseText != null) {
-            console.log(myRequest3.status);
             var data = JSON.parse(myRequest3.responseText);
             if (data.term != "No class for the term") {
                 term += "Winter" + " ";
