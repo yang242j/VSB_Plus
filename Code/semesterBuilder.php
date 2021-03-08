3333ad369d8d114d28b75c114174a7ac9aa1d8fd
yang242j <yang242j@uregina.ca>
2021-03-07 22:11:43 -0600

    Update semesterBuilder.php

--- Code/semesterBuilder.php
+++ Code/semesterBuilder.php
@@ -165,7 +165,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 
                             } else {
                                 // Do nothing and alert the returned Notes
-                                alert(rsp.Notes);
+                                alert(rsp.Notes.bold());
                             }
                         };
                         xhr.send(data);
