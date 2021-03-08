c64b3a582a4bfebbd14aa71ce294bbc80db08d87
xia <huaxiairving@Gmail.com>
2021-03-07 21:41:44 -0600

    test

--- Code/js/academicCustomizeBuilder.js
+++ Code/js/academicCustomizeBuilder.js
@@ -390,18 +390,13 @@ function dragTest() {
             //console.log("dasdasdasd");
             creditsEarned -= getCredits;
             document.getElementById("show_credits").innerHTML = "Credits: " + creditsEarned;
-            
-            dropZone.addEventListener("dragend", e => {
-                //e.preventDefault();
-                dropZone.classList.remove("drop-zone--over");
-                //console.log("dasdasdasd");
-                creditsEarned += getCredits;
-                document.getElementById("show_credits").innerHTML = "Credits: " + creditsEarned;
-    
-    
-            });
-
-
+        });
+        dropZone.addEventListener("dragend", e => {
+            e.preventDefault();
+            dropZone.classList.remove("drop-zone--over");
+            //console.log("dasdasdasd");
+            creditsEarned += getCredits;
+            document.getElementById("show_credits").innerHTML = "Credits: " + creditsEarned;
         });
 
         dropZone.addEventListener("drop", e => {
