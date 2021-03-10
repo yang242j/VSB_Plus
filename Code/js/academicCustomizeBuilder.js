30c52b9a1c3498e56aae429bc8584ef894cfc2a5
xia <huaxiairving@Gmail.com>
2021-03-09 23:53:58 -0600

    test

--- Code/js/academicCustomizeBuilder.js
+++ Code/js/academicCustomizeBuilder.js
@@ -598,7 +598,6 @@ function ajaxpost(courseid, term, done) {
         if (rsp.Prerequisites == true) {
             // Generate course tag
             //courseid = rsp.CourseID;
-            alert("asd");
             return true;
 
         } else {
@@ -610,5 +609,5 @@ function ajaxpost(courseid, term, done) {
     xhr.send(data);
 
     // (C) PREVENT HTML FORM SUBMIT
-    return false;
+    //return false;
 }
