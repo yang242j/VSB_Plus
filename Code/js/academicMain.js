81ad234a96dadabce9ccfec8dc7346b2be45c2c5
yang242j <yang242j@uregina.ca>
2021-03-09 21:30:37 -0600

    Beautify JS

--- Code/js/academicMain.js
+++ Code/js/academicMain.js
@@ -1,16 +1,16 @@
 var summary = {
-    "name":"",
-    "sid" : sid,
-    "program" : "",
-    "major" : major,
+    "name": "",
+    "sid": sid,
+    "program": "",
+    "major": major,
 }
 
 var cmpValue = {
-    "ave" : "",
-    "courseLeft" : "",
-    "year" : "",
-    "gpa" : "",
-    "credit" : "",
+    "ave": "",
+    "courseLeft": "",
+    "year": "",
+    "gpa": "",
+    "credit": "",
 }
 
 var reqCourseNum = 46;
@@ -20,14 +20,14 @@ var reqCredit = 136;
 function pageUp() {
     document.getElementById("card1").innerHTML = "<h3>Program:</h3>" + "<p>" + summary.program + "</p>";
     document.getElementById("card2").innerHTML = "<h3>Year:</h3>" + "<p>" + cmpValue.year + "th</p>";
-    document.getElementById("card3").innerHTML = "<h3>Credit Earned:</h3>" + "<p>" + cmpValue.credit + " / "+ reqCredit + "</p>";
+    document.getElementById("card3").innerHTML = "<h3>Credit Earned:</h3>" + "<p>" + cmpValue.credit + " / " + reqCredit + "</p>";
     document.getElementById("card4").innerHTML = "<h3>Average</h3>" + "<p>" + cmpValue.ave + "</p>";
 }
 
 function pageDown() {
     document.getElementById("card1").innerHTML = "<h3>SID:</h3>" + "<p>" + summary.sid + "</p>";
     document.getElementById("card2").innerHTML = "<h3>Major:</h3>" + "<p>" + summary.major + "</p>";
-    document.getElementById("card3").innerHTML = "<h3>Course Left:</h3>" + "<p>" + cmpValue.courseLeft + " / "+reqCourseNum+"</p>" ;
+    document.getElementById("card3").innerHTML = "<h3>Course Left:</h3>" + "<p>" + cmpValue.courseLeft + " / " + reqCourseNum + "</p>";
     document.getElementById("card4").innerHTML = "<h3>GPA:</h3>" + "<p>" + cmpValue.gpa + "</p>";
 }
 
@@ -41,7 +41,10 @@ window.onload = function () {
 
 function loadPieChart(sid, pas) {
     // console.log(sid);
-    $.post('Model/takenClass.php', { "sid": sid, "password": pas }, function (data) {
+    $.post('Model/takenClass.php', {
+        "sid": sid,
+        "password": pas
+    }, function (data) {
         console.log(data);
         var jsonData = JSON.parse(data);
         var divId = 'pieChart';
@@ -50,7 +53,10 @@ function loadPieChart(sid, pas) {
 }
 
 function loadLineChart(sid, pas) {
-    $.post('Model/takenClass.php', { "sid": sid, "password": pas }, function (data) {
+    $.post('Model/takenClass.php', {
+        "sid": sid,
+        "password": pas
+    }, function (data) {
         console.log(data);
         var jsonData = JSON.parse(data);
         var divId = 'lineChart';
@@ -58,35 +64,39 @@ function loadLineChart(sid, pas) {
     });
 }
 
-function setDefault(sid){
+function setDefault(sid) {
     // console.log(GetUrlRelativePath());
-    $.post('./Api.php/Student/BasicInfo', {'sid': sid}, function(data){
+    $.post('./Api.php/Student/BasicInfo', {
+        'sid': sid
+    }, function (data) {
         // console.log("data is ", data)
         basicInfo = JSON.parse(data).data;
         summary.name = basicInfo.name;
         summary.sid = basicInfo.student_id;
         summary.program = basicInfo.program;
         summary.major = basicInfo.major;
-    });   
+    });
 }
 
-function setCmptedValue(sid){
-    $.post('./Api.php/Student/TakenCourse', {'sid': sid}, function(data){
+function setCmptedValue(sid) {
+    $.post('./Api.php/Student/TakenCourse', {
+        'sid': sid
+    }, function (data) {
         var jsonData = JSON.parse(data).data;
         var sumGrade = 0;
         var passCount = 0;
         var totalCredit = 0;
         var years = [];
         // console.log(jsonData);
-        for (var key in jsonData){
+        for (var key in jsonData) {
             // For computing the which years that student in
             var year = parseInt(jsonData[key].term);
             years.push(year);
-            
+
             // Compiting the averages, learned credits, pass course count.
             var grade = parseInt(jsonData[key].final_grade);
             var earnedCredit = parseInt(jsonData[key].credit_earned);
-            if(!isNaN(grade)){
+            if (!isNaN(grade)) {
                 sumGrade += grade;
                 passCount += 1;
                 totalCredit += earnedCredit;
@@ -104,20 +114,22 @@ function setCmptedValue(sid){
 
         // Refresh the summary borad
         pageDown();
-    });   
+    });
 }
 
 
-　function GetUrlRelativePath()
-　　{
-　　　　var url = document.location.toString();
-　　　　var arrUrl = url.split("//");
+　
+function GetUrlRelativePath()　　 {　　　　
+    var url = document.location.toString();　　　　
+    var arrUrl = url.split("//");
 
-　　　　var start = arrUrl[1].indexOf("/");
-　　　　var relUrl = arrUrl[1].substring(start);//stop省略，截取从start开始到结尾的所有字符
+    　　　　
+    var start = arrUrl[1].indexOf("/");　　　　
+    var relUrl = arrUrl[1].substring(start); //stop省略，截取从start开始到结尾的所有字符
 
-　　　　if(relUrl.indexOf("?") != -1){
-　　　　　　relUrl = relUrl.split("?")[0];
-　　　　}
-　　　　return relUrl;
-　　}
+    　　　　
+    if (relUrl.indexOf("?") != -1) {　　　　　　
+        relUrl = relUrl.split("?")[0];　　　　
+    }　　　　
+    return relUrl;　　
+}
--- Code/js/genChart.js
+++ Code/js/genChart.js
@@ -1,15 +1,15 @@
 function genGraph(sections) {
     //Compute the data for graph
-    var lab_count = [0,0,0];
-    var lecture_count = [0,0,0];
+    var lab_count = [0, 0, 0];
+    var lecture_count = [0, 0, 0];
     var labels_list = ['2020 Spring/Summer', '2020 Fall', '2021 Winter'];
     for (var i in sections) {
         var sec = sections[i];
         // console.log(sec);
-        if (sec.schedule_type == 'Lecture'){
+        if (sec.schedule_type == 'Lecture') {
             var index = labels_list.indexOf(sec.term);
             lecture_count[index] += 1;
-        }else if (sec.schedule_type == 'Lab'){
+        } else if (sec.schedule_type == 'Lab') {
             var index = labels_list.indexOf(sec.term);
             lab_count[index] += 1;
         }
@@ -52,7 +52,7 @@ function genGraph(sections) {
                     // 'rgba(255, 159, 64, 1)'
                 ],
                 borderWidth: 1
-            },{
+            }, {
                 label: 'Number of Lecture',
                 data: lecture_count,
                 backgroundColor: [
@@ -78,7 +78,15 @@ function genGraph(sections) {
                 borderWidth: 1
             }]
         },
-        options: {scales:{yAxes:[{ticks:{beginAtZero: true}}]}}
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
     });
 
 }
@@ -91,89 +99,90 @@ function createCanvas(id, width, height) {
     return canvasNode;
 }
 
-function showGraph(name){
-    var short_name = {'short_name':name};
+function showGraph(name) {
+    var short_name = {
+        'short_name': name
+    };
     $.post('Model/section.php', short_name, function (data, status) {
         genGraph(JSON.parse(data));
         // return JSON.parse(data);
     });
 }
 
-function genChart1(data, divId){
-  //Compute the data for graph
-  var goodGrade = 0;
-  var generalGrade = 0;
-  var poorGrade = 0;
-  var notPass = 0;
-  var total_class = 46;
+function genChart1(data, divId) {
+    //Compute the data for graph
+    var goodGrade = 0;
+    var generalGrade = 0;
+    var poorGrade = 0;
+    var notPass = 0;
+    var total_class = 46;
 
-  // Loop the data and compute the datas.
-  var labels_list = new Set();
-  for (var i in data) {
-      var takenCourse = data[i];
-      labels_list.add(takenCourse.term);
+    // Loop the data and compute the datas.
+    var labels_list = new Set();
+    for (var i in data) {
+        var takenCourse = data[i];
+        labels_list.add(takenCourse.term);
 
-      var grade = takenCourse.final_grade
-      if (grade == 'NP') notPass += 1;
-      if (parseInt(grade) >= 80) goodGrade += 1;
-      if (parseInt(grade) >= 65) generalGrade += 1;
-      if (parseInt(grade) >= 50) poorGrade += 1;
-      
-  }
-  var left_class = total_class - goodGrade - generalGrade - poorGrade; 
+        var grade = takenCourse.final_grade
+        if (grade == 'NP') notPass += 1;
+        if (parseInt(grade) >= 80) goodGrade += 1;
+        if (parseInt(grade) >= 65) generalGrade += 1;
+        if (parseInt(grade) >= 50) poorGrade += 1;
 
-  //In case, there is no 'canvas' element in the html
-  var canvasID = divId + 'Chart';
-  if (!document.getElementById(canvasID)) {
-      $('#' + divId).html(createCanvas(canvasID, 100, 100));
-  }
+    }
+    var left_class = total_class - goodGrade - generalGrade - poorGrade;
 
-  // Get the canvas element to generate the graph
-  var ctx = document.getElementById(canvasID).getContext('2d');
+    //In case, there is no 'canvas' element in the html
+    var canvasID = divId + 'Chart';
+    if (!document.getElementById(canvasID)) {
+        $('#' + divId).html(createCanvas(canvasID, 100, 100));
+    }
 
-  // Set the data and color parameters
-  data = {
-    datasets: [{
-        // data: [goodGrade, generalGrade, poorGrade, notPass, left_class],
-        data: [goodGrade, generalGrade, poorGrade, notPass],
-        backgroundColor: ['#99CC33','#99CCFF',
-        '#FF6666', '#333333','#CCCCCC',
-        // 'rgba(54, 162, 235, 0.2)',
-        // 'rgba(255, 206, 86, 0.2)',
-        // 'rgba(75, 192, 192, 0.2)',
-        // 'rgba(153, 102, 255, 0.2)',
-    ]
-    }],
+    // Get the canvas element to generate the graph
+    var ctx = document.getElementById(canvasID).getContext('2d');
 
-    // These labels appear in the legend and in the tooltips when hovering different arcs
-    labels: [
-        'Good Grade',
-        'Generate',
-        'Poor',
-        'NP',
-        // 'Untaken',
-    ]
-};
+    // Set the data and color parameters
+    data = {
+        datasets: [{
+            // data: [goodGrade, generalGrade, poorGrade, notPass, left_class],
+            data: [goodGrade, generalGrade, poorGrade, notPass],
+            backgroundColor: ['#99CC33', '#99CCFF',
+                '#FF6666', '#333333', '#CCCCCC',
+                // 'rgba(54, 162, 235, 0.2)',
+                // 'rgba(255, 206, 86, 0.2)',
+                // 'rgba(75, 192, 192, 0.2)',
+                // 'rgba(153, 102, 255, 0.2)',
+            ]
+        }],
 
-  var chart = new Chart(ctx, {
-      type: 'doughnut',
-      data: data,
-      options: {}
-  });
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: [
+            'Good Grade',
+            'Generate',
+            'Poor',
+            'NP',
+            // 'Untaken',
+        ]
+    };
+
+    var chart = new Chart(ctx, {
+        type: 'doughnut',
+        data: data,
+        options: {}
+    });
 }
 
-function genChart2(data, divId){
+function genChart2(data, divId) {
     var grade = {};
     var term_list = [];
     for (var i in data) {
         var takenCourse = data[i];
         var final_grade = parseInt(takenCourse.final_grade);
 
-        if (!Number.isNaN(final_grade)){
-            if (term_list.indexOf(takenCourse.term) != -1){
+        if (!Number.isNaN(final_grade)) {
+            if (term_list.indexOf(takenCourse.term) != -1) {
                 grade[takenCourse.term].push(final_grade);
-            }
-            else{
+            } else {
                 term_list.push(takenCourse.term);
                 grade[takenCourse.term] = [final_grade];
             }
@@ -181,13 +190,15 @@ function genChart2(data, divId){
     }
 
     var ave_grades = [];
-    term_list.forEach((term)=>{
+    term_list.forEach((term) => {
         var sum = 0;
         // console.log(term);
-        grade[term].forEach((grade_value)=>{sum += grade_value});
+        grade[term].forEach((grade_value) => {
+            sum += grade_value
+        });
         // console.log(sum);
         // console.log(grade[term].length);
-        ave_grades.push(sum/grade[term].length);
+        ave_grades.push(sum / grade[term].length);
     });
 
 
@@ -196,10 +207,10 @@ function genChart2(data, divId){
     if (!document.getElementById(canvasID)) {
         $('#' + divId).html(createCanvas(canvasID, 150, 100));
     }
-  
+
     // Get the canvas element to generate the graph
     var ctx = document.getElementById(canvasID).getContext('2d');
-  
+
     var chart = new Chart(ctx, {
         type: 'bar',
         data: {
@@ -226,6 +237,14 @@ function genChart2(data, divId){
                 borderWidth: 1
             }]
         },
-        options: {scales:{yAxes:[{ticks:{beginAtZero: true}}]}}
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
     });
 }
