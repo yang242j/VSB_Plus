5d06a44dd92a816c7d19e61c35e531e9723c9d36
xia <huaxiairving@Gmail.com>
2021-03-09 23:14:48 -0600

    test

--- Code/js/academicCustomizeBuilder.js
+++ Code/js/academicCustomizeBuilder.js
@@ -422,7 +422,7 @@ function dragTest() {
             var check = false;
 
             var y = [];
-            y.push(doneList[0][0]);
+            y[0]=(doneList[0][0]);
             //chekc prerequisite
             console.log(ajaxpost("ENEL 280", "202020", y));
 
