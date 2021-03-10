1014df80123735c54a868bbe095ed0404a8ac990
xia <huaxiairving@Gmail.com>
2021-03-09 23:18:48 -0600

    test

--- Code/js/academicCustomizeBuilder.js
+++ Code/js/academicCustomizeBuilder.js
@@ -422,7 +422,7 @@ function dragTest() {
             var check = false;
 
             var y = [];
-            y[0]=doneList[0][0];
+            y[0]=doneList[0][4];
             var x = ["MATH 111", "CS 210"];
             //chekc prerequisite
             console.log(ajaxpost("ENEL 280", "202020", y));
