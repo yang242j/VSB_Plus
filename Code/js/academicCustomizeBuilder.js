3f641bcfb8bd988b3891b9fa17431d5d6c25f6ba
xia <huaxiairving@Gmail.com>
2021-03-10 23:42:56 -0600

    test

--- Code/js/academicCustomizeBuilder.js
+++ Code/js/academicCustomizeBuilder.js
@@ -180,11 +180,10 @@ function showCourses(data) {
 function clickGetInfo(){
     for(i=2;i<5;i++){
         
-        document.getElementById("nct0").addEventListener("dblclick", e => {
+        document.getElementById("nct"+i).addEventListener("dblclick", e => {
             e.preventDefault();
-            alert("asd");
             
-            var courseName = document.getElementById("nnnct0").innerHTML;
+            var courseName = document.getElementById("nnnct"+i).innerHTML;
             const terminfo= getTermInfo(courseName);
     
             if(terminfo[0].length == 0){
