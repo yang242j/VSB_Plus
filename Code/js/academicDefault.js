891ee3dd855b6cb4b6fc638779e1abac86be7511
mingchipc <44107118+mingchipc@users.noreply.github.com>
2021-03-11 16:29:01 -0600

    Update academicDefault.js

--- Code/js/academicDefault.js
+++ Code/js/academicDefault.js
@@ -167,14 +167,14 @@ function nctRight() {
     /*document.getElementById("notCompletedRight").innerHTML = counter;*/
     if (counter >= 0) {
         for (i = 0; i < 12; i++) {
-            document.getElementById("nct" + i).innerHTML = "";
+            document.getElementById("cs" + i).innerHTML = "";
         }
         for (i = 12 * counter; i < 12 * (counter + 1); i++) {
             if (courseData[i] == null) return;
             if (i > courseData.length) {
-                document.getElementById("nct" + j).innerHTML = " ";
+                document.getElementById("cs" + j).innerHTML = " ";
             } else {
-                document.getElementById("nct" + j).innerHTML = courseData[i].short_name;
+                document.getElementById("cs" + j).innerHTML = courseData[i].short_name;
             }
             j = j + 1;
         }
