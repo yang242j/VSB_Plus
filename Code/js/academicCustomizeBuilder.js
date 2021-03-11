ef93e11562c48ae877be43105342e5c47c3978a9
xia <huaxiairving@Gmail.com>
2021-03-10 20:52:03 -0600

    dblclick on cards

--- Code/js/academicCustomizeBuilder.js
+++ Code/js/academicCustomizeBuilder.js
@@ -168,8 +168,13 @@ function showCourses(data) {
     document.getElementById("nct7").addEventListener("dblclick", e => {
         e.preventDefault();
         const terminfo1 = getTermInfo(notCompletedData[7]);
+        if(terminfo1 == null){
+            console.log("not applied");
+        }
+        else{
         info1 = notCompletedData[7] + ":\n" + "Term Applied :" + terminfo1[0] + "\n" + "Credits:" + terminfo1[1] + "\n" + terminfo1[2];
         console.log(info1);
+        }
     });
  
 
