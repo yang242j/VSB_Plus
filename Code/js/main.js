914af7d312b0f147f4188c8802979867f5ca1a5b
yang242j <yang242j@uregina.ca>
2021-03-11 14:06:27 -0600

    Update main.js

--- Code/js/main.js
+++ Code/js/main.js
@@ -1,11 +1,5 @@
-/* Include jQuery */
-var script = document.createElement('script');
-script.src = '//code.jquery.com/jquery-latest.min.js'; 
-document.getElementsByTagName('head')[0].appendChild(script);
-/* End of Include */
-
 var isMobile = window.matchMedia('(max-width: 1080px)').matches
-if (isMobile) $(".menu-icon").click();
+if (isMobile) document.getElementsByClassName("menu-icon")[0].click();
 
 /* When the user clicks on the nav-right buttons,
 toggle between hiding and showing the content with id */
