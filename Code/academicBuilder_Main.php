0128d2d4a802a26eea287a8c6ae003622e5b4589
yang242j <yang242j@uregina.ca>
2021-03-11 18:40:28 -0600

    Update academicBuilder_Main.php

--- Code/academicBuilder_Main.php
+++ Code/academicBuilder_Main.php
@@ -62,7 +62,9 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
         var pas = "<?php echo $_SESSION["password"]; ?>";
         var major = "<?php echo $_SESSION["major"]; ?>";
         var isMobile = window.matchMedia('(max-width: 1080px)').matches;
-        if (isMobile) $(".menu-icon").click();
+        $(document).ready(function() {
+            if (isMobile) $(".menu-icon").click();
+        });
     </script>
     <script type="text/javascript" src="js/academicMain.js"></script>
 </head>
