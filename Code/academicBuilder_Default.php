097850709947a42316591ce13bd2a67992154d5c
yang242j <yang242j@uregina.ca>
2021-03-11 18:28:14 -0600

    Fix favicon.ico 404

--- Code/academicBuilder_Default.php
+++ Code/academicBuilder_Default.php
@@ -44,6 +44,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     <meta name="author" content="team_vsbp">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+	<link rel="shortcut icon" href="#">
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/academicBuilder_Default.css">
 
