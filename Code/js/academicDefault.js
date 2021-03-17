945bd55c0060882b6e7d0e3c9314024921da4366
mingchipc <44107118+mingchipc@users.noreply.github.com>
2021-03-16 21:12:09 -0600

    Update academicDefault.js

--- Code/js/academicDefault.js
+++ Code/js/academicDefault.js
@@ -16,3 +16,68 @@ function getTermData() {
     }
     myRequest.send();
 }
+
+function showTerm(pageNumber) {
+    var i = 1;
+    for (term in termData) {
+        /*console.log(termData[term][0]);*/
+        termNumber = "term" + pageNumber;
+        if (term >= termNumber) {
+            if (term != "Approved") {
+                /*if (i <= 4) {*/
+                if (i <= 10) {
+                    /*if(pageNumber < 7) {*/
+                    if (pageNumber < 12) {
+                        document.getElementById("term" + i).innerHTML =
+                            "<div class = 'tittle'>" + "<h2>" + term + ":" + "</h2></div>" +
+                            "<div class = 'course_cards'>" + "<h3>" + termData[term][0] + "</h3>" +
+                            "<p>" + getTitle(termData[term][0]) + "</p>" +
+                            /*"<i class='fas fa-circle' id = 'circle1' style='font-size:24px;'></i>"+
+                            "<i class='fas fa-circle' style='font-size:24px;color:red'></i>"+
+                            "<i class='fas fa-circle' style='font-size:24px;color:red'></i>"+
+                            "<i class='fas fa-circle' style='font-size:24px;color:red'></i>*/
+                            "</div>" +
+
+
+                            "<div class = 'course_cards'>" + "<h3>" + termData[term][1] + "</h3>" +
+                            "<p>" + getTitle(termData[term][1]) + "</p>" +
+                            /* "<i class='fas fa-circle' style='font-size:24px;color:red'></i>"+
+                             "<i class='fas fa-circle' style='font-size:24px;color:red'></i>"+
+                             "<i class='fas fa-circle' style='font-size:24px;color:red'></i>"+
+                             "<i class='fas fa-circle' style='font-size:24px;color:red'></i>*/
+                            "</div>" +
+
+                            "<div class = 'course_cards'>" + "<h3>" + termData[term][2] + "</h3>" +
+                            "<p>" + getTitle(termData[term][2]) + "</p>" +
+                            /* "<i class='fas fa-circle' style='font-size:24px;color:red'></i>"+
+                             "<i class='fas fa-circle' style='font-size:24px;color:red'></i>"+
+                             "<i class='fas fa-circle' style='font-size:24px;color:red'></i>"+
+                             "<i class='fas fa-circle' style='font-size:24px;color:red'></i>*/
+                            "</div>" +
+
+                            "<div class = 'course_cards'>" + "<h3>" + termData[term][3] + "</h3>" +
+                            "<p>" + getTitle(termData[term][3]) + "</p>" +
+                            /* "<i class='fas fa-circle' style='font-size:24px;color:red'></i>"+
+                             "<i class='fas fa-circle' style='font-size:24px;color:red'></i>"+
+                             "<i class='fas fa-circle' style='font-size:24px;color:red'></i>"+
+                             "<i class='fas fa-circle' style='font-size:24px;color:red'></i>*/
+                            "</div>" +
+
+                            "<div class = 'course_cards'>" + "<h3>" + termData[term][4] + "</h3>" +
+                            "<p>" + getTitle(termData[term][4]) + "</p>" +
+                            /*  "<i class='fas fa-circle' style='font-size:24px;color:red'></i>"+
+                               "<i class='fas fa-circle' style='font-size:24px;color:red'></i>"+
+                               "<i class='fas fa-circle' style='font-size:24px;color:red'></i>"+
+                               "<i class='fas fa-circle' style='font-size:24px;color:yellow'></i>*/
+                            "</div>";
+
+                        "<div class = 'course_cards'>" + "<h3>" + termData[term][5] + "</h3>" +
+                            "<p>" + getTitle(termData[term][5]) + "</p>" + "</div>";
+
+                        i = i + 1;
+                    }
+                }
+            }
+        }
+    }
+}
