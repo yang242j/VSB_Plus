901a1b17e0ca08b367dfb873eb2b8867d433bb48
mingchipc <44107118+mingchipc@users.noreply.github.com>
2021-03-16 21:05:28 -0600

    Update academicDefault.js

--- Code/js/academicDefault.js
+++ Code/js/academicDefault.js
@@ -1,1 +1,18 @@
+var termData;
+var allCourse;
 
+window.onload = function () {
+    showTerm(1);
+}
+
+function getTermData() {
+    var myRequest = new XMLHttpRequest;
+    //myRequest.open("GET", "JSON/reqCourse/ESE_req.json", false);
+    myRequest.open("GET", "JSON/reqCourse/SSE_req.json", false);
+    myRequest.onload = function () {
+        var data = JSON.parse(myRequest.responseText);
+        termData = data;
+        console.log(termData);
+    }
+    myRequest.send();
+}
