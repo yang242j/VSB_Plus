25acf81c46ffdda99e62cda98d43e1e88575f69b
mingchipc <44107118+mingchipc@users.noreply.github.com>
2021-03-17 02:03:16 -0600

    Update academicDefault.js

--- Code/js/academicDefault.js
+++ Code/js/academicDefault.js
@@ -5,6 +5,9 @@ var allCourse;
 window.onload = function () {
     getStuMajor(sid, pas);
     showTerm(1);
+    showENSE_electives();
+    showCS_electives();
+    showENEL_electives();
 }
 
 function getStuMajor(sid, password) {
@@ -130,4 +133,38 @@ function showTerm(pageNumber) {
             }
         }
     }
+}
+
+function showENSE_electives() {
+    for (term in termData) {
+        if (term = "Approved") {
+            for (i = 0; i <= 6; i++) {
+                document.getElementById("ense" + i).innerHTML = termData[term][i];
+            }
+        }
+    }
+}
+
+function showCS_electives() {
+    for (term in termData) {
+        if (term = "Approved") {
+            for (i = 7; i <= 15; i++) {
+                for (i = 0; i < 12; i++) {
+                document.getElementById("cs" + i).innerHTML = termData[term][i];
+            }
+        }
+        }
+    }
+}
+
+function showENEL_electives() {
+    for (term in termData) {
+        if (term = "Approved") {
+            /*for (i = 14; i <= 15; i++) {*/
+            for (i = 0; i < 12; i++) {
+                document.getElementById("ct" + i).innerHTML = termData[term][i];
+            }
+        }
+    }
+
 }
