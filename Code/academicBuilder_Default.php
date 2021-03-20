18e7e434af3e858fbb466922b303d73c7238f128
mingchipc <44107118+mingchipc@users.noreply.github.com>
2021-03-19 19:01:34 -0600

    Update academicBuilder_Default.php

--- Code/academicBuilder_Default.php
+++ Code/academicBuilder_Default.php
@@ -285,10 +285,10 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 			<div class = "electives_course all_course">
 				<h3>All Other Course Electives:</h3>
 				<div class = "course_tag_electives all_course_tag" id = "all_course_tag">
-					<p id = "all0">ENSE 271</p>
-					<p id = "all1">ENSE 271</p>
-					<p id = "all2">ENSE 271</p>
-					<p id = "all3">ENSE 271</p>
+					<p id = "all0"></p>
+					<p id = "all1"></p>
+					<p id = "all2"></p>
+					<p id = "all3"></p>
 
 					<p id = "all4">ENSE 271</p>
 					<p id = "all5">ENSE 271</p>
