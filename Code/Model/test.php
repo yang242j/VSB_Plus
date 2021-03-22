10842c40f91570a4ad92ca8aec184a2e45bd537f
yang242j <yang242j@uregina.ca>
2021-03-22 13:32:16 -0600

    Update test.php

--- Code/Model/test.php
+++ Code/Model/test.php
@@ -2,27 +2,46 @@
 
 $preStr = $expStr = $status = '';
 
+$totalCredit = 10;
+
 $doneList = [
     "CS 110" => 70,
     "MATH 103" => 70,
     // "MATH 110" => 50,
+    "ECON 100" => 50,
+    "Pre-Calculus 20" => 50,
 ];
 
-$preStr = "CS 110 with a minimum grade of 65% and one of MATH 110 (may be taken concurrently) or MATH 103 with a minimum grade of 80%.";
-//$expStr = "CS 110 && ( MATH 110 || MATH 103 )";
-$expStr = "CS 110 [>= 65] && ( MATH 110 || MATH 103 [>= 80] )";
+$preStr = "15 credit hours or ECON 100 or Pre-Calculus 20 (or equivalent)";
+$expStr = "Credit [>= 15] || ECON 100 || Pre-Calculus 20";
+// $expStr = "CS 110 && ( MATH 110 || MATH 103 )";
+// $expStr = "CS 110 [>= 65] && ( MATH 110 || MATH 103 [>= 80] )";
 
 $status = exp_matched($expStr, $doneList) ? "True" : "False";
 
 function exp_matched($expStr, $doneList) {
     $expStr = trim($expStr);
 
-    // Basic: if expStr == null, return true
+    // Base case: if expStr == null, return true
     if ($expStr == null) {
         return true;
     }
 
-    // Basic: exact one course name "ENSE 400"
+    // Base case: Pending credit requirements
+    if (preg_match_all("/(Credit\s\[(.*?)\])/i", $expStr) == 1) {
+        echo "expStr: " . $expStr . "<br>";
+        $creditStr = preg_split("/(\s\[)/i", $expStr);
+        $creditExp = rtrim($creditStr[1], ']');
+        if (eval('return ' . $totalCredit . $creditExp . ';')) {
+            echo "Truee1 <br>";
+            return true;
+        } else {
+            echo "Falsee1 <br>";
+            return false;
+        }
+    }
+
+    // Base case: exact one course name "ENSE 400"
     if (preg_match_all("/([a-z]+\s[0-9]+)/i", $expStr) == 1) {
         // Check if has condition
         if (preg_match_all("/([a-z]+\s[0-9]+\s\[(.*?)\])/i", $expStr) == 1) {
@@ -89,7 +108,7 @@ function exp_matched($expStr, $doneList) {
 <html>
 <body>
 
-<h1> CS 115 </h1>
+<h1> ECO 201 </h1>
 
 Done List: <pre><mark><b><?php print_r($doneList); ?></b></mark></pre><br>
 Prerequisites: <pre><mark><b><?php print_r($preStr); ?></b></mark></pre><br>
