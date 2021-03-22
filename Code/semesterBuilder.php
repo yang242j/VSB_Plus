acd2ddad239a54178ef787a28fd5c20262912254
yang242j <yang242j@uregina.ca>
2021-03-22 14:28:18 -0600

    Update semesterBuilder.php

--- Code/semesterBuilder.php
+++ Code/semesterBuilder.php
@@ -58,9 +58,17 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
         var term = '';
         var courseList = [];
         var isMobile = window.matchMedia('(max-width: 1080px)').matches;
-        let presetCourses = ['Precalculus 30', 'Calculus 30', 'CHEM 30', 'Mathematics B30', 'Mathematics C30', 'AMTH 092', 'MATH 102', 'MATH 103'];
-        var courseCompletedList = [];
-        courseCompletedList = courseCompletedList.concat(presetCourses);
+        let presetCourses = [
+            'Precalculus 30':100, 
+            'Calculus 30':100, 
+            'CHEM 30':100, 
+            'Mathematics B30':100, 
+            'Mathematics C30':100, 
+            'AMTH 092':100, 
+            'MATH 102':100, 
+            'MATH 103':100,
+        ];
+        var courseCompletedList = Object.assign({}, presetCourses);
         
         $(document).ready(function() {
             $("button.plus_button.open").hide();
@@ -270,7 +278,7 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                         require_once "Model/vsbp_db_config.php";
                         $tableName = "S" . $_SESSION['sid'];
                         $takenClass_sql =
-                            "SELECT `course_ID` FROM `" .
+                            "SELECT `course_ID`, `final_grade` FROM `" .
                             $tableName .
                             "` WHERE `credit_earned`=`credit_hour`";
                         $result = mysqli_query($conn, $takenClass_sql);
@@ -279,13 +287,9 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                             exit();
                         }
                         while ($row = mysqli_fetch_array($result)) { ?>
-                            var course_tag = tagGenerator("<?php echo htmlspecialchars(
-                                $row['course_ID']
-                            ); ?>", false);
+                            var course_tag = tagGenerator("<?php echo htmlspecialchars($row['course_ID']); ?>", false);
                             document.getElementById("course_completed").innerHTML += course_tag;
-                            courseCompletedList.push("<?php echo htmlspecialchars(
-                                $row['course_ID']
-                            ); ?>");
+                            courseCompletedList["<?php echo htmlspecialchars($row['course_ID']); ?>"] = "<?php echo htmlspecialchars($row['final_grade']); ?>";
                         <?php }
                         ?>
                     });
