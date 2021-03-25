566153f3b3203ef1bce460ee4641ca9f22a6649f
mingchipc <44107118+mingchipc@users.noreply.github.com>
2021-03-24 22:45:05 -0600

    Update academicBuilder_Default.php

--- Code/academicBuilder_Default.php
+++ Code/academicBuilder_Default.php
@@ -126,14 +126,6 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 						<button class="right_arrow"><i class='fas fa-angle-right'></i></button>
 					</div>-->   
 					
-					<!-- The Modal -->
-					<div id="myModal" class="modal">
-
-  					<!-- Modal content -->
-  					<div class="modal-content">
-    					<span class="close">&times;</span>
-    					<p>Some text in the Modal..</p>
-  					</div>
 
 				</div>
 				</div>
