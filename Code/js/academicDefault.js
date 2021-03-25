4168c2ae2c9982780d0d2a8b2a074479b9d09b57
mingchipc <44107118+mingchipc@users.noreply.github.com>
2021-03-24 22:40:49 -0600

    Update academicDefault.js

--- Code/js/academicDefault.js
+++ Code/js/academicDefault.js
@@ -99,13 +99,13 @@ function showTerm(pageNumber) {
                     if (pageNumber < 12) {
                         document.getElementById("term" + i).innerHTML =
                             "<div class = 'tittle'>" + "<h2>" + term + ":" + "</h2></div>" +
-                            "<button class = 'course_cards' id = 'course_info'>" + "<h3>" + termData[term][0] + "</h3>" +
+                            "<div class = 'course_cards'>" + "<h3>" + termData[term][0] + "</h3>" +
                             "<p>" + getTitle(termData[term][0]) + "</p>" +
                             /*"<i class='fas fa-circle' id = 'circle1' style='font-size:24px;'></i>"+
                             "<i class='fas fa-circle' style='font-size:24px;color:red'></i>"+
                             "<i class='fas fa-circle' style='font-size:24px;color:red'></i>"+
                             "<i class='fas fa-circle' style='font-size:24px;color:red'></i>*/
-                            "</button>" +
+                            "</div>" +
 
 
                             "<div class = 'course_cards'>" + "<h3>" + termData[term][1] + "</h3>" +
@@ -305,28 +305,7 @@ function allRight() {
 }
 
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("course_info");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+function myFunction()
+{
+	alert("I am an alert box!"); // this is the message in ""
 }
