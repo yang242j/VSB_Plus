f1844984c53540f89ecfb5766b13e2d6b3f53634
yang242j <yang242j@uregina.ca>
2021-03-24 19:13:48 -0600

    Update autocomplete.js

--- Code/js/autocomplete.js
+++ Code/js/autocomplete.js
@@ -5,7 +5,7 @@ var countryArray = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguil
 $.getJSON("JSON/ALL.json", function(jsonObjArray) {
     console.log(jsonObjArray);
     /*Excuting the autocomplete function with input values*/
-    autocomplete(document.getElementById("search_courseid"), countryArray);
+    autocomplete(document.getElementById("search_courseid"), jsonObjArray);
 });
 
 /**
@@ -37,17 +37,63 @@ function autocomplete(inputText, array2Check) {
         for (i = 0; i < array2Check.length; i++) {
 
             /*check if the shortName of the course object starts with the same letters as the text field value:*/
-            if (array2Check[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            if (array2Check[i].short_name.substr(0, val.length).toUpperCase() == val.toUpperCase()) {
                 
                 /*create a DIV element for each matching element:*/
                 b = document.createElement("DIV");
                 
                 /*make the matching letters bold:*/
-                b.innerHTML = "<strong>" + array2Check[i].substr(0, val.length) + "</strong>";
-                b.innerHTML += array2Check[i].substr(val.length);
+                b.innerHTML = "<strong>" + array2Check[i].short_name.substr(0, val.length) + "</strong>";
+                b.innerHTML += array2Check[i].short_name.substr(val.length);
                 
                 /*insert a input field that will hold the current array item's value:*/
-                b.innerHTML += "<input type='hidden' value='" + array2Check[i] + "'>";
+                b.innerHTML += "<input type='hidden' value='" + array2Check[i].short_name + "'>";
+                
+                /*execute a function when someone clicks on the item value (DIV element):*/
+                b.addEventListener("click", function(e) {
+                    /*insert the value for the autocomplete text field:*/
+                    inputText.value = this.getElementsByTagName("input")[0].value;
+                    /*close the list of autocompleted values, (or any other open lists of autocompleted values:*/
+                    closeAllLists();
+                });
+                a.appendChild(b);
+            }
+
+            /*check if the shortName of the course object starts with the same letters as the text field value:*/
+            if (array2Check[i].title.substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                
+                /*create a DIV element for each matching element:*/
+                b = document.createElement("DIV");
+                
+                /*make the matching letters bold:*/
+                b.innerHTML = "<strong>" + array2Check[i].title.substr(0, val.length) + "</strong>";
+                b.innerHTML += array2Check[i].title.substr(val.length);
+                
+                /*insert a input field that will hold the current array item's value:*/
+                b.innerHTML += "<input type='hidden' value='" + array2Check[i].short_name + "'>";
+                
+                /*execute a function when someone clicks on the item value (DIV element):*/
+                b.addEventListener("click", function(e) {
+                    /*insert the value for the autocomplete text field:*/
+                    inputText.value = this.getElementsByTagName("input")[0].value;
+                    /*close the list of autocompleted values, (or any other open lists of autocompleted values:*/
+                    closeAllLists();
+                });
+                a.appendChild(b);
+            }
+
+            /*check if the shortName of the course object starts with the same letters as the text field value:*/
+            if (array2Check[i].faculty.substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                
+                /*create a DIV element for each matching element:*/
+                b = document.createElement("DIV");
+                
+                /*make the matching letters bold:*/
+                b.innerHTML = "<strong>" + array2Check[i].faculty.substr(0, val.length) + "</strong>";
+                b.innerHTML += array2Check[i].faculty.substr(val.length);
+                
+                /*insert a input field that will hold the current array item's value:*/
+                b.innerHTML += "<input type='hidden' value='" + array2Check[i].short_name + "'>";
                 
                 /*execute a function when someone clicks on the item value (DIV element):*/
                 b.addEventListener("click", function(e) {
