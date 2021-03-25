3dda08defd075cc2a46e7f722745e73da84ae9c3
xia <huaxiairving@Gmail.com>
2021-03-24 22:33:30 -0600

    customize builder debug

--- Code/academicBuilder_Builder.php
+++ Code/academicBuilder_Builder.php
@@ -110,6 +110,11 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 		<div class="welcome_tag" id = "welcome">
 			<h1 id = "show_credits">Credits:</h1>
 			<h2>Customize Schedule Builder (Not recommended use in touchscreen)</h2>
+			<pre>tips: 1.
+			           2.
+					   3.
+					   4.
+			</pre>
 		</div>
 
 		<section class = "terms" id = "block">
