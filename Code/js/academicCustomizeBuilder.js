fcd3a44724736dd5760a72d021d05dd87e5e0216
xia <huaxiairving@Gmail.com>
2021-03-24 21:27:37 -0600

    customize builder debug

--- Code/js/academicCustomizeBuilder.js
+++ Code/js/academicCustomizeBuilder.js
@@ -69,7 +69,7 @@ function storePassedCourse(data) {
 function getTermData(faculty) {
     var myRequest = new XMLHttpRequest;
     var facultyName = faculty;
-    var url = "JSON/reqCourse/" + facultyName + "_req.json";
+    var url = "JSON/" + facultyName + ".json";
     myRequest.open("GET", url, false);
     myRequest.onload = function () {
         var data = JSON.parse(myRequest.responseText);
