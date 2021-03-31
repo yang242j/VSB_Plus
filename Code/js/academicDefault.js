10f6f4089bb1d8a8d0bacbe4ced05821f2abc206
mingchipc <44107118+mingchipc@users.noreply.github.com>
2021-03-30 20:54:21 -0600

    Update academicDefault.js

--- Code/js/academicDefault.js
+++ Code/js/academicDefault.js
@@ -224,7 +224,7 @@ function showENSE_electives() {
 		document.getElementById("electives").innerHTML = "<div class = 'electives_course'>" + 
 					"<h3>" + electives + ":" + "</h3>" + "</div>";
             /*for (i = 7; i <= 13; i++) {*/
-                for (i = 0; i < 7; i++) {
+                for (i = 0; i <= 6; i++) {
                 	// document.getElementById("ense" + i).innerHTML = electivesData[electives][i];
                 	document.getElementById("e" + i).innerHTML = electivesData[electives][i];
 			
@@ -299,7 +299,7 @@ function showENEL_electives() {
 		document.getElementById("electives").innerHTML = "<div class = 'electives_course'>" + 
 					"<h3>" + electives + ":" + "</h3>" + "</div>";
             /*for (i = 7; i <= 13; i++) {*/
-                for (i = 0; i < 2; i++) {
+                for (i = 0; i < 6; i++) {
                 	// document.getElementById("enel" + i).innerHTML = electivesData[electives][i];
                     document.getElementById("e" + i).innerHTML = electivesData[electives][i];
 			
